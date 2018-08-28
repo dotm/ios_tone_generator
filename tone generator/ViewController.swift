48692be38dbe40cfb33515ab4db5da4526379895
dotm <desde.el.mar96@gmail.com>
2018-08-28 21:05:06 +0700

    Initial Commit

---
+++ tone generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tone generator
+//
+//  Created by Yoshua Elmaryono on 28/08/18.
+//  Copyright © 2018 Yoshua Elmaryono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
